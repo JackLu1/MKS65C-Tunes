17fab6ee7d33869d6ba0e368615a85e2c94e1621
Brian Lee <hlee5@stuy.edu>
2018-10-13 13:18:26 -0400

    Insert songs alphabeticaly

--- song_list.c
+++ song_list.c
@@ -20,25 +20,93 @@ void print_list(struct song_node *n)
 {
     while (n)
     {
-        printf("[%s - %s]\n", n->name, n->artist);
+        printf("[%s - %s]\n", n->artist, n->name);
         n = n->next;
     }
+    printf("\n");
 }
 
 struct song_node *add_song(struct song_node *head, char *artist, char *name)
 {
+    /*
+     * Adds a song before the node, and set its next node to the given node.
+     * (Usually should only be used to add to the front of a list)
+     *
+     * Returns a pointer to the beginning of the list.
+     * 
+     * If the pointer points to the middle of a list, it will break the list unless
+     * the pointer of the previous node is manually set.
+     */
+
     struct song_node *new_head = malloc(sizeof(struct song_node));
     new_head->next = head;
-    n_strncpy(new_head->name, name, 100);
     n_strncpy(new_head->artist, artist, 100);
+    n_strncpy(new_head->name, name, 100);
     return new_head;
 }
 
 struct song_node *add_song_sorted(struct song_node *head, char *artist, char *name)
 {
-    struct song_node *prev = 0;
-    // TODO: FINISH THIS
-    return 0;
+    /*
+     * Adds a song in alphabetical order, first by the artist name and then the song name.
+     *
+     * Returns a pointer to the beginning of the list. 
+     */
+
+    // Empty list, add to head
+    if (head == NULL)
+    {
+        return add_song(head, artist, name);
+    }
+
+    struct song_node *cur;
+    struct song_node *prev;
+    struct song_node *new_node;
+
+    /* Find a position to add the node */
+    for (cur = head, prev = 0; strcmp(artist, cur->artist) > 0; prev = cur, cur = cur->next)
+    {
+        if (cur->next == NULL)
+        {
+            /* Last node reached, add to end */
+            new_node = add_song(0, artist, name);
+            cur->next = new_node;
+            return head;
+        }
+    }
+
+    if (strcmp(artist, cur->artist) < 0)
+    {
+        new_node = add_song(cur, artist, name);
+        if (prev == NULL)
+        {
+            return new_node;
+        }
+        prev->next = new_node;
+        return head;
+    }
+
+    if (strcmp(artist, cur->artist) == 0)
+    {
+        /* Find a position to add the node */
+        for (; strcmp(name, cur->name) > 0; prev = cur, cur = cur->next)
+        {
+            if (cur->next == NULL)
+            {
+                /* Last node reached, add to end */
+                new_node = add_song(0, artist, name);
+                cur->next = new_node;
+                return head;
+            }
+        }
+        new_node = add_song(cur, artist, name);
+        if (prev == NULL)
+        {
+            return new_node;
+        }
+        prev->next = new_node;
+        return head;
+    }
 }
 
 struct song_node *free_list(struct song_node * head)
--- test.c
+++ test.c
@@ -9,35 +9,72 @@ void test_song_list()
     printf(":::Empty list:::\n");
     struct song_node *song_list = 0;
     print_list(song_list);
-    printf("\n");
 
     printf(":::Adding songs to the front:::\n");
     song_list = add_song(song_list, "Red Hot Chili Peppers", "Otherside");
     print_list(song_list);
-    printf("\n");
 
     printf(":::Another one:::\n");
     song_list = add_song(song_list, "Queen", "Bohemeian Rhapsody");
     print_list(song_list);
-    printf("\n");
 
     printf(":::Another one:::\n");
     song_list = add_song(song_list, "John Coltrane", "Blue Train");
     print_list(song_list);
-    printf("\n");
 
     printf(":::Buffer overflow test:::\n");
     song_list = add_song(song_list, 
             "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", 
             "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
     print_list(song_list);
-    printf("\n");
-
+    
+    // NOTE: should be tested with valgrind
     song_list = free_list(song_list);
 }
+
+void test_add_song_sorted()
+{
+    printf("===Testing add_song_sorted()===\n");
+
+    printf(":::Adding to an empty list:::\n");
+    struct song_node *song_list = 0;
+    song_list = add_song_sorted(song_list, "Paul McCartney", "Temporary Secretary");
+    print_list(song_list);
+
+    printf(":::Adding to the beginning:::\n");
+    song_list = add_song_sorted(song_list, "Beatles", "Let it Be");    
+    print_list(song_list);
+
+    printf(":::Adding to the beginning:::\n");
+    song_list = add_song_sorted(song_list, "ABBA", "Dancing Queen");    
+    print_list(song_list);
+
+    printf(":::Adding to the end:::\n");
+    song_list = add_song_sorted(song_list, "Queen", "We Will Rock You");
+    print_list(song_list);
+
+    printf(":::Adding to the end:::\n");
+    song_list = add_song_sorted(song_list, "Vanilla Ice", "Ice Ice Baby");
+    print_list(song_list);
+
+    printf(":::Add by song name, at beginning:::\n");
+    song_list = add_song_sorted(song_list, "Queen", "Don't Stop Me Now");
+    print_list(song_list);
+
+    printf(":::Add by song name, in middle:::\n");
+    song_list = add_song_sorted(song_list, "Queen", "Killer Queen");
+    print_list(song_list);
+
+    printf(":::Add by song name, at end of list:::\n");
+    song_list = add_song_sorted(song_list, "Vanilla Ice", "Play That Funky Music");
+    print_list(song_list);
+}
+
 
 int main()
 {
     test_song_list();
+    test_add_song_sorted();
+    return 0;
 }
 
