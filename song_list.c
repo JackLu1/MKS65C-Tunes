3e329ece0d9cd1fc8d50751b43f4562ef40b0f34
Brian Lee <hlee5@stuy.edu>
2018-10-13 12:25:52 -0400

    .gitignore

--- song_list.c
+++ song_list.c
@@ -18,32 +18,37 @@ char *n_strncpy(char * dest, char * src, int len)
 
 void print_list(struct song_node *n)
 {
-    while(n)
+    while (n)
     {
         printf("[%s - %s]\n", n->name, n->artist);
         n = n->next;
     }
 }
 
-struct song_node *add_song(struct song_node *front, char *name, char *artist)
+struct song_node *add_song(struct song_node *head, char *artist, char *name)
 {
-    struct song_node * new_front = malloc(sizeof(struct song_node));
-    new_front->next = front;
-    n_strncpy(new_front->name, name, 100);
-    n_strncpy(new_front->artist, artist, 100);
-    return new_front;
+    struct song_node *new_head = malloc(sizeof(struct song_node));
+    new_head->next = head;
+    n_strncpy(new_head->name, name, 100);
+    n_strncpy(new_head->artist, artist, 100);
+    return new_head;
 }
 
-struct song_node *free_list(struct song_node * front)
+struct song_node *add_song_sorted(struct song_node *head, char *artist, char *name)
 {
-    struct song_node * temp = front;
-    struct song_node * ret = front;
-    while(temp)
-    {
-        temp = temp->next;
-        free(front);
-        front = temp;
-    }
+    struct song_node *prev = 0;
+    // TODO: FINISH THIS
+    return 0;
+}
 
-    return ret;
+struct song_node *free_list(struct song_node * head)
+{
+    struct song_node *temp = head;
+    while (head)
+    {
+        temp = head;
+        free(temp);
+        head = head->next;
+    }
+    return 0;
 }
